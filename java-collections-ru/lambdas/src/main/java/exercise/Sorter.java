b9a3c93e2c26887b6d70dd323888ccfdc361c92f
@hexlet/cli <support@hexlet.io>
2022-12-10 13:48:45 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,5 +1,6 @@
 package exercise;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,6 +8,9 @@ import java.util.stream.Collectors;
 // BEGIN
 class Sorter {
     public static List<String> takeOldestMans(List<Map<String, String>> peoples) {
+        if (peoples.isEmpty()) {
+            return new ArrayList<>(0);
+        }
         return peoples.stream()
                 .filter(name -> name.get("gender").equals("male"))
                 .map(name -> name.get("age"))
